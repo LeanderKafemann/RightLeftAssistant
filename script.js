66a5916558f679766a4842ef94d7f3d70450b456
leander@kafemann.berlin <leander@kafemann.berlin>
2025-09-04 12:34:41 +0200

    test new encoding

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
+﻿document.addEventListener("DOMContentLoaded", function () {
     const leftBtn = document.getElementById("leftBtn");
     const rightBtn = document.getElementById("rightBtn");
     const info = document.getElementById("info");
 
     leftBtn.addEventListener("click", function () {
-        info.textContent = "<-- Links ist dort";
+        info.textContent = "← Links ist dort";
         leftBtn.style.background = "#1976d2";
         rightBtn.style.background = "#ffb74d";
     });
 
     rightBtn.addEventListener("click", function () {
-        info.textContent = "Rechts ist dort -->";
+        info.textContent = "Rechts ist dort →";
         rightBtn.style.background = "#1976d2";
         leftBtn.style.background = "#4fc3f7";
     });
